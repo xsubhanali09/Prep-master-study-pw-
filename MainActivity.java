c5dedbf6306403630440f179fc602f05c3731b40
xsubhanali09 <xsubhanali09@gmail.com>
2026-09-05 13:06:49 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,38 @@
+package com.prepmaster.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private static final String HOME_URL = "https://pwthor.live/auth";
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(HOME_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Prep Master Android
+
+A simple Android WebView wrapper for the configured website.
+
+## Build on GitHub
+1. Upload this whole project to a GitHub repository.
+2. Open **Actions**.
+3. Select **Build Prep Master APK**.
+4. Click **Run workflow**.
+5. Open the successful run and download the **Prep-Master-debug** artifact.
+
+App name: Prep Master
+Package: com.prepmaster.app
+Version: 1.0
+Home URL: https://pwthor.live/auth
+
+Use this wrapper only if you have permission to package/use the configured website.
